26a92d17b96a1ced3b6f304ae2278cb03b2400a6
htomcat <thamada@yahoo-corp.jp>
2017-10-14 22:17:30 +0900

    [modify]change rootViewController

--- CleanSwiftTableView/CleanSwiftTableView/AppDelegate.swift
+++ CleanSwiftTableView/CleanSwiftTableView/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
-        window?.rootViewController = CleanSwiftViewController()
+        let nc = UINavigationController(rootViewController: CleanSwiftViewController())
+        window?.rootViewController = nc
         window?.makeKeyAndVisible()
         return true
     }
